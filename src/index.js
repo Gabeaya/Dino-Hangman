796925eeb0143acb23a7f33b08a2ba85d1ef1612
Gabriel Ayala <gabeayala100@gmail.com>
2021-08-04 19:33:15 -0700

    Add a ui function and import the dino js.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-/*import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import any business logic with the following syntax:
-import [BusinessObject] from './triangle.js';
-for multiple imports from the same file follow this syntax:
-import[{*/
+import Dino from './dino.js'
 
+$(document).ready(function() {
+  $('#').click(function() {
+    let userParagraph = $('#paragraph').val();
+    $('#paragraph').val("");
+    let promise = Dino.getDinos();
+    promise.then(function(response) {
+      const body =JSON.parse(reponse);
+      $()
+    })
+  })
+})
 
 
